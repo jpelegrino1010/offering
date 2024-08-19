0b01261ccb8c5e84d9ea44bd4eca19f6790fc2b5
Julio Luis Pelegrino <julio.java02@gmail.com>
2024-08-19 16:13:01 -0400

    Inital commit

---
+++ src/main/java/com/julioluis/offering/OfferingApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.offering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferingApplication.class, args);
+	}
+
+}
